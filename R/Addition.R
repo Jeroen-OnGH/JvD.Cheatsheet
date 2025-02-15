9a37c0a6da0c38149e155e75b267d3c9bc8599ee
Jeroen-OnGH <spamenbevestigingen@protonmail.com>
2025-02-15 14:29:30 +0100

    Addition as practice function

---
+++ R/Addition.R
@@ -0,0 +1,14 @@
+#' Addition
+#'
+#' @param x First value
+#' @param y Second value
+#'
+#' @returns z sum of values
+#' @export
+#'
+#' @examples
+#' #Output = 3
+#' Addition(1,2)
+Addition <- function(x,y){
+  return(x+y)
+}
