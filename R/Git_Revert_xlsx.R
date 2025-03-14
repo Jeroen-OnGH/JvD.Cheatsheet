e5b28bcbf6956aed4e2e9893994344c989e73918
Jeroen-OnGH <spamenbevestigingen@protonmail.com>
2025-03-14 12:38:44 +0100

    Git_Reverse toegevoegd

---
+++ R/Git_Revert_xlsx.R
@@ -0,0 +1,46 @@
+#Dit is bedoeld als hoofdstuk om git revert uit te leggen en waarom het niet werkt met excel.
+
+library(tidyverse)
+library(gert)
+
+#Maak een nieuwe test branch:
+gert::git_branch_create("revert_xlsx_csv")
+
+#Maken Bron bestand----
+Revert_Data <- tibble::tibble(
+  Changes = c("Basis")
+)
+write.csv2(Revert_Data,"data-raw/Revert_Data.csv")
+xlsx::write.xlsx(Revert_Data,"data-raw/Revert_Data.xlsx")
+
+git_add(".")
+git_commit("Maken Bron")
+
+#Eerste Aanpassing----
+
+Revert_Data <- Revert_Data %>%
+  add_row(Changes = "Verandering 1")
+
+write.csv2(Revert_Data,"data-raw/Revert_Data.csv")
+xlsx::write.xlsx(Revert_Data,"data-raw/Revert_Data.xlsx")
+
+git_add(".")
+git_commit("Verandering 1")
+
+#Tweede Aanpassing----
+
+Revert_Data <- Revert_Data %>%
+  add_row(Changes = "Verandering 2")
+
+write.csv2(Revert_Data,"data-raw/Revert_Data.csv")
+xlsx::write.xlsx(Revert_Data,"data-raw/Revert_Data.xlsx")
+
+git_add(".")
+git_commit("Verandering 2")
+
+#Opschonen
+
+git_add(".")
+git_commit("Afronden Revert")
+git_branch_checkout("master")
+git_branch_delete("revert_xlsx_csv")
